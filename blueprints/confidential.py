7c452eed6348ec5ceb649b5979a5670e47eb4c36
Pranav Teja ☑️ <86045290+Pranavtej@users.noreply.github.com>
2023-10-15 14:57:06 +0530

    Add files via upload

---
+++ blueprints/confidential.py
@@ -0,0 +1,21 @@
+# Add your own credentials
+
+MONGOURI = ""
+
+IBM_ENDPOINT = ""
+
+IBM_API_KEY_ID = ""
+
+IBM_INSTANCE_CRN = ""
+
+IBM_ACCESS_KEY_ID = ""
+
+IBM_SECRET_ACCESS_KEY = ""
+
+REDIS_URI = ""
+
+REDIS_HOST = ""
+
+REDIS_PADDWORD = ""
+
+OPEN_AI_KEY = ""
